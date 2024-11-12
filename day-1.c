75005085397b494451344fd89d3ad69c77b0a5b7
Md1o1 <kelvinmendes8@gmail.com>
2024-11-11 23:50:11 -0300

    setup

---
+++ day-1.c
@@ -0,0 +1,9 @@
+/***********************************************************************************************
+ *> Propósito: sistema gerencial para um cinema
+ *> Objetivo: compreender a sintaxe do uso de matriz e conceitos de modularização na linguagem C
+ *> Autor: Md1o1 - Kelvin Mendes
+***********************************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
